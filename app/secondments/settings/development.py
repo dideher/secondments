c5122e480dcd1180fb3969839ce6fd9d331d53e6
Filip Slavik <filippos@slavik.gr>
2022-11-05 20:31:04 +0200

    initial commit

---
+++ app/secondments/settings/development.py
@@ -0,0 +1,10 @@
+from secondments.settings.common import *
+
+
+ALLOWED_HOSTS = ['*']
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = '/app/static_files'
+MEDIA_ROOT = '/app/media_files'
+
